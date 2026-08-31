d3d1a6eac59832fb03b3d5ddd9369f7eeb036da9
Olive Oparaocha <oliveifeoma@gmail.com>
2026-08-31 02:19:08 +0000

    style(infra): terraform fmt

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,11 +1,10 @@
 terraform {
-   required_version = ">= 1.11"
+  required_version = ">= 1.11"
 
-   backend "s3" {
-     bucket         = "phoenix-tfstate-2b08529f"
-     key            = "phoenix/terraform.tfstate"
-     region         = "eu-north-1"
-     encrypt        = true
-     use_lockfile   = true
-   }   
-}
+  backend "s3" {
+    bucket       = "phoenix-tfstate-2b08529f"
+    key          = "phoenix/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
+  }
