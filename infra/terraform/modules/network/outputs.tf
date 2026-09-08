975a473d0fbe97a515ed911ebdcc3ab68b1c0adc
Olive Oparaocha <oliveifeoma@gmail.com>
2026-09-08 12:26:56 +0000

    feat(infra): expose VPC and subnet IDs from network module

---
+++ infra/terraform/modules/network/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
