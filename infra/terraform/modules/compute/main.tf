c2dfaf86eeda24e95633fc2a0caae183b6c53217
Olive Oparaocha <oliveifeoma@gmail.com>
2026-09-13 17:58:36 +0000

    feat(infra): provision k3s server and worker instances

---
+++ infra/terraform/modules/compute/main.tf
@@ -0,0 +1,57 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "cluster" {
+  key_name   = "${var.project}-key"
+  public_key = file(pathexpand(var.ssh_public_key_path))
+}
+
+resource "aws_instance" "server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group_placeholder]
+  key_name               = aws_key_pair.cluster.key_name
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.project}-server"
+    Role = "k3s-server"
+  }
+}
+
+resource "aws_instance" "worker" {
+  count = var.worker_count
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = aws_key_pair.cluster.key_name
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.project}-worker-${count.index}"
+    Role = "k3s-agent"
+  }
+}
